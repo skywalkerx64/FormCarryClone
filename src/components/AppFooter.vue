df7755d041f2535908b2a9b2dcf080442195797b
skywalkerx64 <rickjoaodonald@gmail.com>
2025-02-05 22:11:23 +0100

    feat : app footer basis

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import Button from '@/components/ui/button/Button.vue'
+</script>
+<template>
+  <footer class="w-full container p-4 grid grid-cols-1 sm:grid-cols-2 sm:gap-8 md:grid-cols-4 gap-4 justify-start items-start">
+    <div>
+      <img src="@/assets/logo.svg" alt="logo" />
+      <div class="flex my-4 gap-4 items-center">
+        <a href=""><img src="@/assets/icons/facebook.svg" alt="facebook" /></a>
+        <a href=""><img src="@/assets/icons/instagram.svg" alt="instagram" /></a>
+        <a href=""><img src="@/assets/icons/twitter.svg" alt="twitter" /></a>
+      </div>
+    </div>
+    <div>
+      <ul class="flex flex-col gap-4">
+        <li class="font-bold">Quick Links</li>
+        <li><a href="">Terms</a></li>
+        <li><a href="">Privacy Policy</a></li>
+        <li><a href="">Support</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul class="flex flex-col gap-4">
+        <li class="font-bold">Follow Us</li>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Twitter</a></li>
+        <li><a href="">Facebook</a></li>
+      </ul>
+    </div>
+    <div>
+      <p class="font-bold">Subscribe to our newsletter</p>
+      <p>Stay updated with the latest TwerkNet news</p>
+      <div class="mt-4 flex">
+        <Input class="h-full" placeholder="Email adress" />
+        <Button variant="outline" class="ml-2">Subscribe</Button>
+      </div>
+    </div>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue'
+import ToggleTheme from '@/components/Utils/ToggleTheme.vue'
+</script>
 
 <template>
+  <ToggleTheme />
   <body class="bg-muted/10">
     <span class="">Hello</span>
   </body>
+  <AppFooter />
 </template>
